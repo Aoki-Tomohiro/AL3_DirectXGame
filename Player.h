b5cc86f56a470a51cb080e46d46e469846eb57a4
青木智滉 <@MSI>
2023-05-12 16:40:15 +0900

    02_02_確認課題

--- Player.h
+++ Player.h
@@ -18,4 +18,8 @@ private:
 	uint32_t textureHandle_ = 0u;
 	//キーボード入力
 	Input* input_ = nullptr;
+	//ImGuiで値を入力する変数
+	float* inputFloat3[3] = {
+	    &worldTransform_.translation_.x, &worldTransform_.translation_.y,
+	    &worldTransform_.translation_.z};
 };
