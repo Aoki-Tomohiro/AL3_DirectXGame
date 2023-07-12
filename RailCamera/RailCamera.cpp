cc7eaad53ff806c229891f7c48bd2d45661d0d59
青木智滉 <@MSI>
2023-07-12 15:01:52 +0900

    02_12_確認課題

--- RailCamera/RailCamera.cpp
+++ RailCamera/RailCamera.cpp
@@ -12,8 +12,8 @@ void RailCamera::Initialize(Vector3 translation, Vector3 rotation){
 
 void RailCamera::Update() { 
 	Vector3 move{0, 0, 0}; 
-	const float kCameraSpeed = 0.05f;
-	const float kRotSpeed = 0.002f;
+	const float kCameraSpeed = 0.02f;
+	const float kRotSpeed = 0.0004f;
 
 	// 行列の更新
 	move.z -= kCameraSpeed;
